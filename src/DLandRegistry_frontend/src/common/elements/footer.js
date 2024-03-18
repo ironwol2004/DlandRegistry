d5b92f5b969b693f19d6ec21f4b69c43a50e4e89
Deepak <deepakdmr456@gmail.com>
2024-03-18 18:10:01 +0530

    Home and Form pages are done

--- src/DLandRegistry_frontend/src/common/elements/footer.js
+++ src/DLandRegistry_frontend/src/common/elements/footer.js
@@ -1,8 +1,11 @@
 import React from "react";
+import logo from "../../images/logo.jpg";
 function Footer(){
-    return <footer className="bg-purple-900 flex space-x-3 justify-center py-4">
-        <h4 className="text-white">Email : decentralisedlandregisrtry@dmail.com </h4>
-        <h4 className="text-white">Contact no. : 9999955555 </h4>
+    return <footer className="bg-blue-400 flex space-x-3 justify-center py-4">
+        <div style={{ display: 'flex', justifyContent: 'flex-start'}}>
+            <img class= "h-7 w-7 rounded-full" src= {logo} alt="Web Logo" />
+        </div>
+        <h4 className="text-white">© Decentralised Land Registry 2024</h4>
     </footer>
 }
 export default Footer;
