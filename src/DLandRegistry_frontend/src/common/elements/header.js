c20ed7f01f84706e716998d0f6f5c6f4e68c2708
ironwol <shrivastavashish2005@gmail.com>
2024-03-24 11:01:24 +0530

    navlink

--- src/DLandRegistry_frontend/src/common/elements/header.js
+++ src/DLandRegistry_frontend/src/common/elements/header.js
@@ -1,14 +1,14 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 function Header(){
     return <nav className="bg-purple-900 flex justify-between py-4">
         <h3 className="font-extrabold text-lg px-6 text-white">DLR</h3>
         <ul className="flex space-x-6 px-6">
-            <Link to="/" className="text-white hover:cursor-pointer hover:text-slate-500">Home</Link>
-            <Link to="/getyourpropertydetails" className="text-white hover:cursor-pointer hover:text-slate-500">Get details of my property</Link>
-            <Link to="/addmyproperty" className="text-white hover:cursor-pointer hover:text-slate-500">Add my property</Link>
-            <Link to="/allproperties" className="text-white hover:cursor-pointer hover:text-slate-500">All Properties</Link>
-            <Link to="/about" className="text-white hover:cursor-pointer hover:text-slate-500">About us</Link>
+            <NavLink to="/" className="text-white hover:cursor-pointer hover:text-slate-500 active:bg-white active:border-purple-800 active:border-2 active:text-purple-950" exact>Home</NavLink>
+            <NavLink to="/getyourpropertydetails" className="text-white hover:cursor-pointer hover:text-slate-500">Get details of my property</NavLink>
+            <NavLink to="/addmyproperty" className="text-white hover:cursor-pointer hover:text-slate-500">Add my property</NavLink>
+            <NavLink to="/allproperties" className="text-white hover:cursor-pointer hover:text-slate-500">All Properties</NavLink>
+            <NavLink to="/about" className="text-white hover:cursor-pointer hover:text-slate-500">About us</NavLink>
         </ul>
     </nav>
 }
